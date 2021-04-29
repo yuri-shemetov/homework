3ba7312acfced5169a352c2e60851813c1ad7329
Shemetov <yuri-sh@tut.by>
2021-04-29 21:08:53 +0300

    Watch (v.1.1)

---
+++ Watch/digits.py
@@ -0,0 +1,67 @@
+# Список цифр
+b = "🟨" 
+s = "\u0020"
+one = [2*s + b + b + 4*s,
+    4*s + b + 4*s,
+    4*s + b + 4*s,
+    4*s + b + 4*s,
+    b + b + b + b + b]
+
+
+two = [b + b + b + b + b, 
+    8*s + b,
+    b + b + b + b + b,
+    b + 8*s,
+    b + b + b + b + b]
+
+three = [b + b + b + b + b,
+    8*s + b,
+    b + b + b + b + b,
+    8*s + b,
+    b + b + b + b + b]
+
+four = [b + 6*s + b,
+    b + 6*s + b,
+    b + b + b + b + b,
+    8*s + b,
+    8*s + b]
+
+five = [b + b + b + b + b,
+    b + 8*s,
+    b + b + b + b + b,
+    8*s + b,
+    b + b + b + b + b]
+
+
+six = [b + b + b + b + b,
+    b + 8*s,
+    b + b + b + b + b,
+    b + 6*s + b,
+    b + b + b + b + b]
+
+
+seven = [b + b + b + b + b,
+    8*s + b,
+    6*s + b + 2*s,
+    4*s + b + 4*s,
+    2*s + b + 6*s]
+
+    
+eight = [b + b + b + b + b,
+    b + 6*s + b,
+    b + b + b + b + b,
+    b + 6*s + b,
+    b + b + b + b + b]
+
+nine = [b + b + b + b + b,
+    b + 6*s + b,
+    b + b + b + b + b,
+    8*s + b,
+    b + b + b + b + b]
+
+
+zero = [b + b + b + b + b,
+    b + 6*s + b,
+    b + 6*s + b,
+    b + 6*s + b,
+    b + b + b + b + b]
